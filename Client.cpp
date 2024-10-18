04cc91bdd9007aa691daac42f6b793a5970154aa
Aurélien <aurelien.litra@gmail.com>
2024-10-18 14:30:05 +0200

    Initialisation de la class Client

---
+++ Client.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <Client.h>
+#include <string>
+
+
+Client::Client(std::string prenom, std::string nom) {
+	_prenom = prenom;
+	_nom = nom;
+}
---
+++ Client.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+class Client {
+public:
+	std::string prenom;
+	std::string nom;
+private:
+	std:string _prenom;
+	std::string _nom;
+};
